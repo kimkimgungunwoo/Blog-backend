567503520ed0b69ce0dfc953e5c70db4b9b935be
kimkimgungunwoo <gunwoo7179@gmail.com>
2025-04-23 16:36:07 +0900

    post썸네일 코드 수정

--- src/main/java/com/example/blogex/domain/post/dto/PostThumbnailResponse.java
+++ src/main/java/com/example/blogex/domain/post/dto/PostThumbnailResponse.java
@@ -2,11 +2,23 @@ package com.example.blogex.domain.post.dto;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class PostThumbnailResponse {
     private Long postId;
     private String thumbnailUrl;
+    private String thumbnailOcr;
+
+    @Builder
+    public PostThumbnailResponse(Long postId,
+                                 String thumbnailUrl,
+                                 String thumbnailOcr) {
+        this.postId = postId;
+        this.thumbnailUrl = thumbnailUrl;
+        this.thumbnailOcr = thumbnailOcr;
+    }
 }
