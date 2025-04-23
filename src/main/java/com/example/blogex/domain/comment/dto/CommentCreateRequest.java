4dce3edcc0b8aaf416eeaeb18bb6f34029defbf6
kimkimgungunwoo <gunwoo7179@gmail.com>
2025-04-23 23:33:57 +0900

    dto수정

--- src/main/java/com/example/blogex/domain/comment/dto/CommentCreateRequest.java
+++ src/main/java/com/example/blogex/domain/comment/dto/CommentCreateRequest.java
@@ -8,15 +8,13 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class CommentCreateRequest {
-    private Long userId;
     private String content;
     private Long parentCommentId;
 
     @Builder
-    public CommentCreateRequest(Long userId,
-                                String content,
+    public CommentCreateRequest(String content,
                                 Long parentCommentId) {
-        this.userId = userId;
+
         this.content = content;
         this.parentCommentId = parentCommentId;
     }
