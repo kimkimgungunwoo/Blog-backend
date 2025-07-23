5161212720dd837785129d146d758d28e3b38a94
kimkimgungunwoo <gunwoo7179@gmail.com>
2025-07-23 15:19:54 +0900

    swagger 추가 완료

--- src/main/java/com/example/blogex/BlogexApplication.java
+++ src/main/java/com/example/blogex/BlogexApplication.java
@@ -1,10 +1,11 @@
 package com.example.blogex;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.mapstruct.Mapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
+@OpenAPIDefinition
 @SpringBootApplication
 @EnableJpaAuditing
 public class BlogexApplication {
